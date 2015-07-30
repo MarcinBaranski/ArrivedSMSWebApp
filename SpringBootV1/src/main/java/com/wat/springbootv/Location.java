9c078f0a2a2a262afe1175ea067eb65dfb2d93e6
ArrivedSMSWebApp <mbaranski92@gmail.com>
2015-07-30 12:13:15 +0200

    Dodany,adres ip serwera i obsluga bledu z brakiem lokalizacji na poczatku startu serwera.

--- SpringBootV1/src/main/java/com/wat/springbootv/Location.java
+++ SpringBootV1/src/main/java/com/wat/springbootv/Location.java
@@ -5,18 +5,21 @@ import org.springframework.stereotype.Component;
 
 public class Location {
 
+	
+
 	private String longitude;
 	private String latitude;
-	
+	private String ipServer;
 	
 	public Location() {
 		super();
 	}
 
-	public Location(String longitude, String latitude) {
+	public Location(String longitude, String latitude,String ipServer) {
 		super();
 		this.longitude = longitude;
 		this.latitude = latitude;
+		this.ipServer = ipServer;
 	}
 
 	public String getLongitude() {
@@ -39,6 +42,14 @@ public class Location {
 
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
+	}
+
+	public String getIpServer() {
+		return ipServer;
+	}
+
+	public void setIpServer(String ipServer) {
+		this.ipServer = ipServer;
 	}
 	
 	
--- SpringBootV1/src/main/java/com/wat/springbootv/LocationShowController.java
+++ SpringBootV1/src/main/java/com/wat/springbootv/LocationShowController.java
@@ -1,5 +1,8 @@
 package com.wat.springbootv;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan
 public class LocationShowController {
 
+	String serverIP;
 	Location loc = new Location();
 
 	@Autowired
@@ -28,8 +32,32 @@ public class LocationShowController {
 	@ModelAttribute("loc")
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String greeting(Model model) {
-		model.addAttribute("latitude", loc.getLatitude().toString());
-		model.addAttribute("longitude", loc.getLongitude().toString());
-		return "hello";
+		if(loc.getLatitude()==null||loc.getLongitude()==null){
+			model.addAttribute("latitude", "Brak aktualnego po³o¿enia");
+			model.addAttribute("longitude", "Brak aktualnego po³o¿enia");
+			return "hello";
+		}else{
+			model.addAttribute("latitude", loc.getLatitude().toString());
+			model.addAttribute("longitude", loc.getLongitude().toString());
+			getServerIP();
+			model.addAttribute("ipServer", serverIP);
+			return "hello";
+		}
+	}
+
+	public void getServerIP(){
+		InetAddress ip;
+		String hostname;
+		try {
+			ip = InetAddress.getLocalHost();
+			serverIP = ip.getHostAddress();
+			//			hostname = ip.getHostName();
+			//			System.out.println("Your current IP address : " + ip);
+			//			System.out.println("Your current Hostname : " + hostname);
+
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+		}
 	}
 }

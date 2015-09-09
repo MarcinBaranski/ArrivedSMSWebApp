7f6b9051aa3474edd944e448fb103c31310b6911
ArrivedSMSWebApp <mbaranski92@gmail.com>
2015-09-09 22:33:03 +0200

    Very Important annotations added :)) Works

--- SpringBootV1/src/main/java/com/wat/springbootv/LocationUpdateListener.java
+++ SpringBootV1/src/main/java/com/wat/springbootv/LocationUpdateListener.java
@@ -1,29 +1,24 @@
 package com.wat.springbootv;
 
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Component
-@RestController
 public class LocationUpdateListener {
 	
-//	@Autowired
-//	private SimpMessagingTemplate template;
+	private SimpMessagingTemplate template;
 	
-//	@Autowired
-//	public LocationUpdateListener(SimpMessagingTemplate template) {
-//		super();
-//		this.template = template;
-//	}
+	@Autowired
+	public LocationUpdateListener(SimpMessagingTemplate template) {
+		super();
+		this.template = template;
+	}
 
-	@RequestMapping("/locationUpdate")
-	@SendTo("/topic/updates")
 	public Location onLocationUpdate(Location location){
 		System.out.println("listener got new location: " + location);
-//		String url = "/topic/updates";
-//		template.convertAndSend(url ,location);
+		String url = "/topic/updates";
+		template.convertAndSend(url ,location);
 		return location;
 	}
 }

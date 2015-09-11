04193fcbef57397904ef3e6909c612ca3633b7a5
ArrivedSMSWebApp <mbaranski92@gmail.com>
2015-09-11 13:01:31 +0200

    Wyświetlanie lokalizacji na www (WIP)

--- SpringBootV1/src/main/resources/static/js/location-socket.js
+++ SpringBootV1/src/main/resources/static/js/location-socket.js
@@ -10,38 +10,37 @@
         function connect() {
             var socket = new SockJS('/locationUpdate', null, 
             		{
-            			debug: true,
             			protocols_whitelist: ['websocket', 'xdr-streaming', 'xhr-streaming', 'xdr-polling', 'xhr-polling', 'iframe-eventsource', 'jsonp-polling']
             		});
             console.log("trying to connect...");
             stompClient = Stomp.over(socket);
             stompClient.connect({}, function(frame) {
-                //setConnected(true);
+                setConnected(true);
                 console.log('Connected: ' + frame);
-                stompClient.subscribe('/topic/updates', function(greeting){
-                	console.log("!!!!!!!!!!!!!! got it!!!!!!!!");
-                    showGreeting(JSON.parse(greeting.body).content);
+                stompClient.subscribe('/topic/updates', function(location){
+                	console.log("!!!!!!!!!!!!!! got it!!!!!!!! ", location.body);
+                    showGreeting(JSON.parse(location.body));
                 });
             });
         }
 
-        function disconnect() {
-            if (stompClient != null) {
-                stompClient.disconnect();
-            }
-            setConnected(false);
-            console.log("Disconnected");
-        }
+//        function disconnect() {
+//            if (stompClient != null) {
+//                stompClient.disconnect();
+//            }
+//            setConnected(false);
+//            console.log("Disconnected");
+//        }
 
-        function sendName() {
-            var name = document.getElementById('name').value;
-            stompClient.send("/app/locationUpdate", {}, JSON.stringify({ 'name': name }));
-        }
+//        function sendName() {
+//            var name = document.getElementById('name').value;
+//            stompClient.send("/app/locationUpdate", {}, JSON.stringify({ 'name': name }));
+//        }
 
         function showGreeting(message) {
             var response = document.getElementById('response');
             var p = document.createElement('p');
             p.style.wordWrap = 'break-word';
-            p.appendChild(document.createTextNode(message));
+            p.appendChild(document.createTextNode(message.longitude));
             response.appendChild(p);
         }
